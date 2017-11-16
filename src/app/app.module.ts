0554b045435ba0dc78d205931331f36ce942fad8
Jorcelino Junior <jorcelino@live.com>
2017-11-16 13:45:10 -0200

    aula 47
    antes de começar a aula 47

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule} from './app-routing.module';
+import { TarefasModule } from './tarefas';
 
 
 @NgModule({
@@ -10,7 +12,9 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    TarefasModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/tarefas/listar/listar-tarefa.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-listar-tarefa',
+  templateUrl: './listar-tarefa.component.html',
+  styleUrls: ['./listar-tarefa.component.css'],
+  encapsulation: ViewEncapsulation.None
+})
+export class ListarTarefaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
